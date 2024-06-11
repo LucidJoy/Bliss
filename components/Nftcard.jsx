55fbd5098b4d445c842e2e160c99263d33b7427a
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2024-06-11 23:55:24 +0530

    minor item update

--- components/Nftcard.jsx
+++ components/Nftcard.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Nftcard = ({ text, btnName }) => {
+const Nftcard = ({ text, btnName, clicked }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(clicked);
+  };
+
   return (
-    <div className='bg-[#2C2C2C] rounded-lg p-4 w-[250px] flex flex-col items-center justify-center space-y-2 transition-transform duration-300 ease-in-out hover:scale-105'>
+    <button
+      className='bg-[#2C2C2C] rounded-lg p-4 w-[250px] flex flex-col items-center justify-center space-y-2 transition-transform duration-300 ease-in-out hover:scale-105'
+      onClick={handleClick}
+    >
       <img
         alt={text}
         height='120'
@@ -15,13 +25,10 @@ const Nftcard = ({ text, btnName }) => {
         width='120'
       />
       <div className='text-sm font-medium syne'>{text}</div>
-      <Link
-        className='inline-flex h-8 items-center justify-center rounded-md bg-[#FF6B6B] px-4 text-sm font-medium text-white shadow transition-colors hover:bg-[#FF6B6B]/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#FF6B6B] disabled:pointer-events-none disabled:opacity-50 syne'
-        href='#'
-      >
+      <p className='inline-flex h-8 items-center justify-center rounded-md bg-[#FF6B6B] px-4 text-sm font-medium text-white shadow transition-colors hover:bg-[#FF6B6B]/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#FF6B6B] disabled:pointer-events-none disabled:opacity-50 syne'>
         {btnName}
-      </Link>
-    </div>
+      </p>
+    </button>
   );
 };
 
--- components/Recommendations.jsx
+++ components/Recommendations.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import Nftcard from "./Nftcard";
+import { useRouter } from "next/router";
 
 const Recommendations = () => {
+  const router = useRouter();
+
   return (
     <div className='flex flex-col items-center justify-center gap-[15px]'>
       <div className='inline-block rounded-lg bg-[#FF6B6B] px-3 py-1 text-sm syne w-fit'>
@@ -17,9 +20,9 @@ const Recommendations = () => {
       </p>
 
       <div className='mx-auto grid items-start gap-12 grid-cols-3 w-fit mt-[30px]'>
-        <Nftcard text='Whimsical Wonders' btnName='Explore' />
-        <Nftcard text='Ethereal Enigmas' btnName='Explore' />
-        <Nftcard text='Futuristic Fusions' btnName='Explore' />
+        <Nftcard text='Whimsical Wonders' btnName='Explore' clicked='item/1' />
+        <Nftcard text='Ethereal Enigmas' btnName='Explore' clicked='item/1' />
+        <Nftcard text='Futuristic Fusions' btnName='Explore' clicked='item/1' />
       </div>
     </div>
   );
